dfb70caabc24e5364b70ae15da3d9e4d1c408095
Dennis Prangle <dennis.prangle@gmail.com>
2016-01-24 17:40:44 +0000

    Version from home laptop

---
+++ init.el
@@ -0,0 +1,8 @@
+(put 'dired-find-alternate-file 'disabled nil)
+(custom-set-variables
+ '(LaTeX-command "latex")
+ '(TeX-PDF-mode t)
+ '(ansi-color-names-vector ["black" "#d55e00" "#009e73" "#f8ec59" "#0072b2" "#cc79a7" "#56b4e9" "white"])
+ '(custom-enabled-themes (quote (deeper-blue))))
+(require 'ess-site)
+(ess-toggle-underscore nil)
