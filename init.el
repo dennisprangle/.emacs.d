9ac74dc3dbb5546649d8f8ce41eb9472f819c660
Dennis Prangle <dennis.prangle@gmail.com>
2016-03-15 12:21:47 +0000

    Unicode options

--- init.el
+++ init.el
@@ -6,3 +6,12 @@
  '(custom-enabled-themes (quote (deeper-blue))))
 (require 'ess-site)
 (ess-toggle-underscore nil)
+
+;;;;;;;;;;;;;;;;;;;;
+;;; set up unicode
+(prefer-coding-system       'utf-8)
+(set-default-coding-systems 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(setq default-buffer-file-coding-system 'utf-8)
+(setq x-select-request-type '(UTF8_STRING COMPOUND_TEXT TEXT STRING))
